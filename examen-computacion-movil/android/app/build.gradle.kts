688cc8a8f4e278f46884677da06b1d9acdf3b1ee
Ayleen Urzua <158379712+leenss6@users.noreply.github.com>
2025-05-07 22:27:21 -0400

    Primer commit del proyecto de computación móvil

--- examen-computacion-movil/android/app/build.gradle.kts
+++ examen-computacion-movil/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.td3"
+    namespace = "com.example.examen-computacion-movil"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.td3"
+        applicationId = "com.example.examen-computacion-movil"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- examen-computacion-movil/test/widget_test.dart
+++ examen-computacion-movil/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:td3/main.dart';
+import 'package:examen-computacion-movil/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
